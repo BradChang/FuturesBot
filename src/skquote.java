723d99924d4789ffa9387b42f96af813fe53f8c2
lwhuang <lw.member@gmail.com>
2015-01-15 07:23:16 +0800

    add quote and osquote jar

---
+++ src/skquote.java
@@ -0,0 +1,20 @@
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class skquote
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
